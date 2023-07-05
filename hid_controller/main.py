9bb07d50fbf7a37147878d8e4b7b4153720fcd39
keszinaj <kubajaniszek123@gmail.com>
2023-07-05 16:22:20 +0300

    first try

--- hid_controller/main.py
+++ hid_controller/main.py
@@ -1,26 +1,5 @@
 from key_mapping import key_mapped
 import time
-while 1:
-    print("Wybierz co chcesz zrobić\n1. Payload 1\n2.Payload 2\n3.Payload 3\n4. New payload\n")
-    x = input()
-    if(x == 1):
-       None
-    elif(x == 2):
-       None
-    elif(x == 3):
-       None
-    elif(x == '4'):
-       y = []
-       inp = ''
-       print("Enter code:\n")
-       while (inp != 'END' and inp != 'ENDQ'):
-          inp = input()
-          y.append(inp)
-       
-    else:
-       print("Wrong value")
-       time.sleep(4)
-    
 
 '''
 {
@@ -36,6 +15,7 @@ ENDQ --exit without execution
 def hid_write(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
+
 class Command:
   def __init__(self, t, val):
     self.c_type = t
@@ -75,4 +55,33 @@ def my_parser(one_cmd):
 
 def my_eval(exe_cmd):
    for obj in exe_cmd:
+      for l in obj.c_val:
+         hid_write(key_mapped[l])
+         hid_write(chr(0)*8)
       print(obj.c_val)
+      print(obj.c_type)
+
+while 1:
+    print("Wybierz co chcesz zrobić\n1. Payload 1\n2.Payload 2\n3.Payload 3\n4. New payload\n")
+    x = input()
+    if(x == '1'):
+       None
+    elif(x == '2'):
+       None
+    elif(x == '3'):
+       None
+    elif(x == '4'):
+       y = []
+       inp = ''
+       print("Enter code:\n")
+       while (inp != 'END' and inp != 'ENDQ'):
+          inp = input()
+          y.append(inp)
+       time.sleep(10)
+       my_lexspars(y)
+       
+    else:
+       print("Wrong value")
+       time.sleep(4)
+    
+

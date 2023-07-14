05addf1275f5f8409f56451031136dc00e8468f4
keszinaj <kubajaniszek123@gmail.com>
2023-07-14 15:52:31 +0300

    add alt b

--- hid_controller/keyMapping.py
+++ hid_controller/keyMapping.py
@@ -91,6 +91,7 @@ key_mapped = {
 ']': chr(2)+chr(0)+chr(79)+chr(0)*5,
 '[': chr(2)+chr(0)+chr(80)+chr(0)*5,
 'CTRLALTDEL': chr(5)+chr(0)+chr(76)+chr(0)*5,
+'ALTb':chr(4)+chr(0)+chr(5)+chr(0)*5,
 
 #Next Signs are used for different keymappings - change if you want to
 '$': chr(0)*2 +chr(40)+chr(0)*5, #ENTER
@@ -103,3 +104,4 @@ key_mapped = {
 #']': chr(5)+chr(0)+chr(76)+chr(0)*5, #CTRL+ALT+DEL
 '|': chr(0)*2 +chr(81)+chr(0)*5, #DOWN
 }
+
